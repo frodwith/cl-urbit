24f7260c36c45bd2530dd2aa32cb20bb782924fa
Paul Driver <frodwith@gmail.com>
2019-09-21 22:56:52 -0700

    less trivial tests for interner

--- t/interner.lisp
+++ t/interner.lisp
@@ -1,14 +1,26 @@
 (defpackage urbit/tests/interner
   (:use :cl :rove)
+  (:import-from :urbit/noun :noun)
+  (:import-from :urbit/cell :tail)
   (:import-from :urbit/context :make-context :with-context :intern-noun))
 
 (in-package :urbit/tests/interner)
 
+(defun in (&rest args)
+ (intern-noun (noun args)))
+
 (deftest interner
  (with-context (make-context)
   (testing "fixnums"
-   (ok (eq 1 (intern-noun 1)))
-   (ok (eq 42 (intern-noun 42)))
-   (ok (eq most-positive-fixnum (intern-noun most-positive-fixnum))))))
+   (ok (eq 1 (in 1)))
+   (ok (eq 42 (in 42)))
+   (ok (eq most-positive-fixnum (in most-positive-fixnum))))
+  (testing "bigatoms"
+   (ok (eq (in #xdeadbeefcafebabefeefeefee)
+           (in #xdeadbeefcafebabefeefeefee))))
+  (testing "cells"
+   (ok (eq (in 1 2) (in 1 2))))
+   (ok (eq (in 1 2 3 4 5) (in 1 2 3 4 5)))
+   (ok (eq (tail (in 1 2 3)) (in 2 3)))))
 
 (run-suite *package*)
