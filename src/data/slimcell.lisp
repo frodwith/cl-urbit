ee3fb723ac587403d6a84b48ded41d091ffc44dd
Paul Driver <frodwith@gmail.com>
2019-09-28 08:17:32 -0700

    make slimcells structs instead of standard objects

--- src/data/slimcell.lisp
+++ src/data/slimcell.lisp
@@ -1,6 +1,6 @@
 (defpackage urbit/data/slimcell
   (:use :cl)
-  (:import-from :urbit/noun :to-noun)
+  (:import-from :urbit/noun :to-noun :noun)
   (:import-from :urbit/equality :teach)
   (:import-from :urbit/formula :formula)
   (:import-from :urbit/context :intern-noun)
@@ -10,97 +10,89 @@
 
 (in-package :urbit/data/slimcell)
 
-(defclass slimcell ()
-  ((head :initarg :head
-         :accessor shead
-         :type noun)
-   (tail :initarg :tail
-         :accessor stail
-         :type noun)
-   (meta :initarg :mug
-         :initform nil
-         :accessor smeta)))
+(defstruct (slimcell (:constructor scons (head tail))
+                     (:print-object print-slimcell))
+  (head nil :type noun)
+  (tail nil :type noun)
+  (meta nil))
 
 (defmethod cellp ((a slimcell))
   t)
 
 (defmethod head ((a slimcell))
-  (shead a))
+  (slimcell-head a))
 
 (defmethod tail ((a slimcell))
-  (stail a))
+  (slimcell-tail a))
 
 (defun in (a &optional mug)
   (let ((i (intern-noun a mug)))
-    (setf (shead a) (chead i))
-    (setf (stail a) (ctail i))
-    (setf (smeta a) i)))
+    (setf (slimcell-head a) (chead i))
+    (setf (slimcell-tail a) (ctail i))
+    (setf (slimcell-meta a) i)))
 
 (defmethod formula ((a slimcell))
   (formula
-    (let ((m (smeta a)))
+    (let ((m (slimcell-meta a)))
       (etypecase m
         (null (in a))
         (fixnum (in a m))
         (constant-cell m)))))
 
 (defmethod cached-mug ((a slimcell))
-  (let ((m (smeta a)))
+  (let ((m (slimcell-meta a)))
     (etypecase m
       (null nil)
       (fixnum m)
       (constant-cell (cmug m)))))
 
 (defmethod compute-mug ((a slimcell))
-  (let ((m (smeta a)))
+  (let ((m (slimcell-meta a)))
     (etypecase m
-      (null (setf (smeta a) (mug-cell a)))
+      (null (setf (slimcell-meta a) (mug-cell a)))
       (fixnum nil)
       (constant-cell (cmug m)))))
 
 (defmethod learn-mug ((a slimcell) (mug fixnum))
-  (let ((m (smeta a)))
+  (let ((m (slimcell-meta a)))
     (etypecase m
-      (null (setf (smeta a) mug))
+      (null (setf (slimcell-meta a) mug))
       (fixnum nil)
       (constant-cell nil))))
 
 (defmethod learn-head ((a slimcell) (head t))
-  (setf (shead a) head))
+  (setf (slimcell-head a) head))
 
 (defmethod learn-tail ((a slimcell) (tail t))
-  (setf (stail a) tail))
+  (setf (slimcell-tail a) tail))
 
 (defmethod learn-constant-cell ((a slimcell) (k constant-cell))
-  (setf (smeta a) k))
+  (setf (slimcell-meta a) k))
 
 (defmethod get-constant-cell ((a slimcell))
-  (let ((m (smeta a)))
+  (let ((m (slimcell-meta a)))
     (etypecase m
       (null nil)
       (fixnum nil)
       (constant-cell m))))
 
 (defun teach-meta (a b)
-  (let ((m (smeta a)))
+  (let ((m (slimcell-meta a)))
     (etypecase m
       (null nil)
       (fixnum (learn-mug b m))
       (constant-cell (teach m b)))))
 
 (defmethod teach ((a slimcell) (b t))
-  (learn-head b (shead a))
-  (learn-tail b (stail a))
+  (learn-head b (slimcell-head a))
+  (learn-tail b (slimcell-tail a))
   (teach-meta a b))
 
 (defmethod unify ((a slimcell) (b slimcell))
-  (setf (shead b) (shead a))
-  (setf (stail b) (stail a))
+  (setf (slimcell-head b) (slimcell-head a))
+  (setf (slimcell-tail b) (slimcell-tail a))
   (teach-meta a b)
   (teach-meta b a))
-
-(defun scons (head tail)
-  (make-instance 'slimcell :head head :tail tail))
 
 (defmethod to-noun ((a cons))
   (let* ((head (car a))
@@ -110,5 +102,5 @@
         here
         (scons here (to-noun tail)))))
 
-(defmethod print-object ((a slimcell) out)
-  (print-cell (shead a) (stail a) out))
+(defun print-slimcell (a out)
+  (print-cell (slimcell-head a) (slimcell-tail a) out))
