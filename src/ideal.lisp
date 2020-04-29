3b4e6468f0f60a85321db16d5223578c4359c599
Paul Driver <frodwith@gmail.com>
2020-04-28 18:04:09 -0700

    going to remove kernel knowledge of stencils

--- src/ideal.lisp
+++ src/ideal.lisp
@@ -1,5 +1,6 @@
 (defpackage #:urbit/ideal
   (:use #:cl #:urbit/data #:urbit/mug #:urbit/control)
+  (:import-from #:urbit/math #:uint)
   (:export #:make-world #:find-ideal #:imug #:iint
            #:iatom #:iatom-mug #:iatom-int #:iatom=mugatom
            #:icell #:icell-mug #:icell-head #:icell-tail #:icell-meta
@@ -56,6 +57,8 @@
   (roots nil :type (maps integer stencil))
   (parents nil :type (maps stencil stencil)))
 
+(deftype driver () (or null function))
+
 (defstruct (formula (:constructor make-formula (form)))
   (form nil :read-only t :type (or list symbol))
   (func nil :type (or null function)))
@@ -105,7 +108,8 @@
   (int nil :read-only t :type bignum)
   (mug nil :read-only t :type mug))
 
-(deftype ideal () '(or fixnum iatom icell))
+(deftype ideal-atom () '(or fixnum iatom))
+(deftype ideal () '(or ideal-atom icell))
 
 (defun iint (i)
   (etypecase i
@@ -124,7 +128,7 @@
   (declare (ideal i))
   (etypecase i
     (icell t)
-    ((or fixnum iatom) nil)))
+    (ideal-atom) nil))
 
 (defun iatom=mugatom (i a)
   (or (eql i a) ; takes care of actual fixnums (no cached mug)
@@ -186,13 +190,19 @@
 
 (sb-ext:define-hash-table-test atoms= atoms-hash)
 
-(defstruct (world (:constructor make-world ()))
+(defstruct (world (:constructor init-world ()))
   "the context in which uniqueness is ensured"
   ; split into two tables because
   ; 1) smaller tables = faster lookups
   ; 2) distinguished groups = faster hash/comparison
   (atoms (make-hash-table :test 'atoms= :weakness :key) :read-only t)
-  (cells (make-hash-table :test 'cells= :weakness :key) :read-only t))
+  (cells (make-hash-table :test 'cells= :weakness :key) :read-only t)
+  (roots (make-hash-table :test 'equal) :read-only t))
+
+(defun make-world (&optional jet-tree)
+  (let ((w (init-world)))
+    (when jet-tree (install-tree jet-tree))
+    w))
 
 (defun hashed-ideal (table noun)
   (let ((found (gethash noun table)))
@@ -225,6 +235,11 @@
              icell)))
     (sum-cell mugged #'atomic #'fast #'slow)))
 
+(defun find-cons (world ihead itail)
+  (let ((findr (icons ihead itail (murmugs (imug ihead) (imug itail)))))
+    (hash-place p (world-cells-world) findr
+      (or p (setq p findr)))))
+
 (defun find-ideal (world noun)
   (if (deep noun)
       (let ((cells (world-cells world)))
@@ -233,3 +248,168 @@
       (let ((atoms (world-atoms world)))
         (or (hashed-ideal atoms noun)
             (create-iatom atoms noun)))))
+
+; kernels are used to link drivers to stencils
+; driver takes stencil constructor args minus driver,
+; produces nil or function from axis to core function
+
+(defstruct kernel
+  (name nil :type uint :read-only t)
+  (driver nil :type (or null function) :read-only t)
+  (children (make-hash-table :test 'equal) :read-only t)
+  (stencils (make-hash-table :test 'eq) :read-only t))
+
+(defstruct (root (:constructor make-root (constant name driver))
+                 (:include kernel))
+  (constant nil :type uint :read-only t))
+
+(defmacro hash-place (place-symbol hash-form key-form &body forms)
+  (let ((h (gensym)) (k (gensym)))
+    `(let ((,h ,hash-form)
+           (,k ,key-form))
+       (symbol-macrolet ((,place-symbol (gethash ,k ,h))) 
+         ,@forms))))
+
+(defmacro root-place (root-symbol world name constant &body forms)
+  `(hash-place ,root-symbol (world-roots ,world) (cons ,constant ,name)
+     ,@forms))
+
+; install functions are called during world initialization.
+; TODO: devise a data structure we can pass to the world
+; constructor that will call this, and don't export it
+; (can still call from test)
+
+(define-condition reinstall-kernel (error) (kernel))
+
+(defun install-root (world name constant driver)
+  (root-place r world name constant
+    (if r
+        (error 'reinstall-kernel r)
+        (setq r (make-root constant name driver)))))
+
+(defun find-root (world name constant)
+  (root-place r world name constant
+    (or r (setq r (make-root constant name nil)))))
+
+(defstruct (child (:constructor make-static (parent name driver)
+                  (:include kernel)))
+  (parent nil :type kernel :read-only t))
+
+(defstruct (dynamic (:constructor make-dynamic (parent name axis driver))
+                    (:include child))
+  (axis nil :type uint :read-only t))
+
+(defun make-child (name axis parent driver)
+  (if (and (= 1 axis) (kernel-static parent))
+      (make-static name parent driver)
+      (make-dynamic name axis parent driver)))
+
+(defmacro child-place (child-symbol parent name axis &body forms)
+  `(hash-place ,child-symbol (kernel-children ,parent) (cons ,axis ,name)
+     ,@forms))
+
+(defun install-child (parent name axis driver)
+  (child-place c parent name axis
+    (if c
+        (error 'reinstall-kernel :kernel c)
+        (setq c (make-child parent name axis driver)))))
+
+(defun find-child (parent name axis)
+  (child-place c parent name axis
+    (or c (setq c (make-child parent name axis nil)))))
+
+(defun kernel-static (kernel)
+  (not (typep kernel 'dynamic)))
+
+(defun parent-axis (kernel)
+  (etypecase kernel
+    (dynamic (dynamic-axis kernel))
+    (child 1)))
+
+(defstruct stencil
+  (ideal nil :type ideal) ; battery or static core (see kernel)
+  (hooks nil :type ideal) ; unprocessed hook list
+  (kernel nil :type kernel)
+  (driver nil :type (or null function)))
+
+(defstruct (child-stencil (:include stencil))
+  (parent nil :type stencil))
+
+(defmacro stencil-place (stencil-symbol kernel ideal &body forms)
+  `(hash-place ,stencil-symbol (kernel-stencils ,kernel) ,ideal
+     ,@forms))
+
+(defun call-kernel-driver (kernel ideal hooks)
+  (let ((kdriver (kernel-driver kernel)))
+    (when kdriver
+      (funcall kdriver kernel ideal hooks))))
+
+; once you get the returned stencil, you presumably will use it to update
+; the battery's match maps etc.
+
+(defun install-root-stencil (world icore name hooks)
+  (let* ((constant (icell-tail icore))
+         (kernel (find-root world name constant)))
+    (stencil-place s kernel icore
+      (if s
+          (error 'stencil-reinstall :stencil s)
+          (setq s (make-stencil
+                    :ideal icore :kernel kernel
+                    :driver (call-kernel-driver kernel icore hooks)))))))
+
+(defun install-child-stencil (world parent battery name axis hooks)
+  (let* ((parentk (stencil-kernel parent))
+         (kernel (find-child parentk name axis))
+         (ideal (if (kernel-static kernel)
+                    (find-cons world battery (stencil-ideal parent))
+                    battery)))
+    (stencil-place s kernel ideal
+      (if s
+          (error 'stencil-reinstall :stencil s)
+          (setq s (make-child-stencil
+                    :ideal ideal :kernel kernel :parent parent
+                    :driver (call-kernel-driver kernel ideal hooks)))))))
+
+(defstruct jet-core
+  (name nil :type uint)
+  (driver nil :type (or null function))
+  (children nil :type list))
+
+(defstruct (jet-root
+             (:constructor root (name constant driver &optional children))
+             (:include jet-core))
+  (constant nil :type uint))
+
+(defstruct (jet-child
+             (:constructor core (name axis driver &optional children))
+             (:include jet-core))
+  (axis nil :type uint))
+
+(defun install-jet-child (world parent-kernel child)
+  (install-jet-children
+    world child (install-child
+                  world parent-kernel
+                  (jet-core-name child)
+                  (find-ideal world (jet-child-axis child))
+                  (jet-core-driver child))))
+
+(defun install-jet-children (world jet-parent parent-kernel)
+  (dolist ((child (jet-core-children jet-parent)))
+    (install-jet-child world parent-kernel child)))
+
+(defun install-jet-root (world root)
+  (install-jet-children
+    world root (install-root
+                 world
+                 (jet-core-name root)
+                 (find-ideal world (jet-root-constant root))
+                 (jet-core-driver root))))
+
+(defun install-tree (world roots)
+  (dolist (r roots) (install-jet-root world r)))
+
+; kernels are installed at boot time by the equivalent of jets/tree.c
+; stencils are installed by 
+;   1) the battery-pack process 
+;   2) fast hints (when enabled) after a core clocks slow
+; stencil installation causes clocking to find matching cores
---
+++ t/nock.lisp
@@ -0,0 +1,149 @@
+(defpackage #:urbit/tests/nock
+  (:use #:cl #:fiveam #:urbit/tests #:urbit/syntax #:urbit/nock 
+        #:urbit/equality #:urbit/data))
+
+(in-package #:urbit/tests/nock)
+
+(def-suite nock-tests
+           :description "test nock"
+           :in all-tests)
+
+(def-suite basic-nock-tests
+           :description "test the t-shirt functionality (no jets) of nock"
+           :in nock-tests)
+
+(def-suite jet-tests
+           :description "test jets"
+           :in nock-tests)
+
+(in-suite basic-nock-tests)
+
+(enable-syntax)
+
+(test autocons
+  (bottle
+    (let ((r (nock [0 42] [[0 3] 0 2])))
+      (is (= 42 (nock r [0 2])))
+      (is (= 0 (nock r [0 3]))))))
+
+(test fragment
+  (bottle
+    (let ((trel [1 2 3]))
+      (is (= 1 (nock trel [0 2])))
+      (is (= 2 (nock trel [0 6])))
+      (is (= 3 (nock trel [0 7]))))))
+
+(test quoted
+  (bottle
+    (is (same 42 (nock 0 [1 42])))
+    (is (same #xdeadbeefcafebabefeefeefee
+              (nock 0 [1 #xdeadbeefcafebabefeefeefee])))
+    (is (same [0 42] (nock 0 [1 0 42])))))
+
+(test nock
+  (bottle
+    (is (= 42 (nock [[0 2] 42 0] [2 [0 3] 0 2])))))
+
+(test deep
+  (bottle
+    (is (= 1 (nock 3 [3 0 1])))
+    (is (= 0 (nock [0 0] [3 0 1])))))
+
+(test bump
+  (bottle
+    (is (= 1 (nock 0 [4 0 1])))
+    (is (= 42 (nock 41 [4 0 1])))
+    (is (same (nock most-positive-fixnum [4 0 1]) (1+ most-positive-fixnum)))))
+
+(test same
+  (bottle
+    (is (= 0 (nock [42 42] [5 [0 2] 0 3])))
+    (is (= 1 (nock [42 43] [5 [0 2] 0 3])))
+    (is (= 0 (nock 0 [5 [1 42] 1 42])))))
+
+(test basic-if
+  (bottle
+    (let ((forty-or-two [6 [0 1] [1 40] 1 2]))
+      (is (= 40 (nock 0 forty-or-two)))
+      (is (= 2 (nock 1 forty-or-two)))
+      (signals exit (nock 2 forty-or-two))
+      (signals exit (nock [0 0] forty-or-two)))))
+
+(test funky-if
+  (bottle
+    (let ((funky [6 [0 1] 1 1 42]))
+      (is (= 42 (nock 1 funky)))
+      (signals exit (nock 0 funky)))))
+
+(test compose
+  (bottle
+    (is (same [2 1] (nock [[1 2] 3] [7 [0 2] [0 3] 0 2])))))
+
+(test tislus
+  (bottle
+    (is (same [0 42] (nock 0 [8 [1 42] [0 3] 0 2])))))
+
+(test pull
+  (bottle
+    (is (= 42 (nock 0 [9 2 1 [0 6] 42 0])))))
+
+(test edit
+  (bottle
+    (is (same [1 3 3] (nock [1 2 3] [10 [6 4 0 6] 0 1])))
+    (let ((set-head [10 [2 1 42] 0 1]))
+      (is (same [42 0] (nock [0 0] set-head)))
+      (signals exit (nock 0 set-head)))
+    (signals exit (nock 0 [10 [0 1 42] 0 1]))
+    (is (= 42 (nock [0 0 0] [10 [1 1 42] 0 1])))
+    (signals exit (nock 0 [10 [1 1 42] 0 0]))))
+
+(test hint
+  (bottle
+    (is (= 42 (nock 42 [11 1 0 1])))
+    (is (= 42 (nock 42 [11 [1 1 1] 0 1])))
+    (signals exit (nock 42 [11 [1 0 0] 0 1]))))
+
+(in-suite jet-tests)
+
+;  =<  ack
+;  =>  %kack
+;  ~%  %kack  ~  ~
+;  |%
+;  ++  dec
+;    ~/  %dec
+;    |=  a=@
+;    =|  i=@
+;    |-
+;    =+  n=+(i)
+;    ?:  =(n a)
+;      i
+;    $(i n)
+;  ++  ack
+;    ~/  %ack
+;    |=  [m=@ n=@]
+;    ?~  m  +(n)
+;    ?~  n  $(m (dec m), n 1)
+;    $(m (dec m), n $(n (dec n)))
+;  --
+(defparameter +ackerman-source+ [7 [7 [1 %kack] 8 [1 [7 [8 [1 0] [1 8 [1 0] 8 [1 8 [4 0 6] 6 [5 [0 2] 0 62] [0 14] 9 2 10 [6 0 2] 0 3] 9 2 0 1] 0 1] 11 [%fast 1 %dec [0 7] 0] 0 1] 7 [8 [1 0 0] [1 6 [5 [1 0] 0 12] [4 0 13] 6 [5 [1 0] 0 13] [9 2 10 [6 [8 [9 4 0 7] 9 2 10 [6 0 28] 0 2] 1 1] 0 1] 9 2 10 [6 [8 [9 4 0 7] 9 2 10 [6 0 28] 0 2] 9 2 10 [13 8 [9 4 0 7] 9 2 10 [6 0 29] 0 2] 0 1] 0 1] 0 1] 11 [%fast 1 %ack [0 7] 0] 0 1] 11 [%fast 1 %kack [1 0] 0] 0 1] 9 5 0 1])
+
+;(defparameter *dec-jet-invocations* 0)
+;
+;(defun dec-jet (sam)
+;  (incf *dec-jet-invocations*)
+;  (let ((i (cl-integer sam)))
+;    (if (= 0 i)
+;      (error 'exit)
+;      (1- i))))
+
+;(defstruct )
+;
+;(root %kack
+;  (gate dec #'dec-jet))
+
+(defun ack (n m)
+  (bottle
+    (nock [n m] [9 2 10 [6 0 1] +ackerman-source+])))
+
+(test ack
+  (is (= 7 (ack 2 2))))
