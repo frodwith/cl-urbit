6a9d614b0cf2119a392ad55c3b986bb1132dc04d
Paul Driver <frodwith@gmail.com>
2020-06-04 14:33:53 -0700

    initial version of core data type

--- src/data/icell.lisp
+++ src/data/icell.lisp
@@ -6,23 +6,24 @@
 (defmethod deep ((p icell))
   t)
 
-(defmethod head ((a icell))
-  (icell-head a))
-
-(defmethod unique-head ((a icell))
-  (icell-head a))
-
-(defmethod tail ((a icell))
-  (icell-tail a))
-
 (defmethod cached-mug ((a icell))
   (icell-mug a))
 
 (defmethod cached-ideal ((a icell))
   a)
 
+(defmethod head ((a icell))
+  (icell-head a))
+
+(defmethod tail ((a icell))
+  (icell-tail a))
+
+(defmethod cached-battery ((a icell))
+  (icell-head a))
+
 (defmethod cached-speed ((c icell))
   (icell-speed c))
 
 (defmethod (setf cached-speed) (val (c icell))
   (setf (icell-speed c) val))
+
